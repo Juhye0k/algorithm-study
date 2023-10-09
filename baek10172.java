8f61f48a22e4249202f1390ee6f3ffb143f1b63b
juhyeok <96466824+Juhye0k@users.noreply.github.com>
2023-10-09 21:00:23 +0900

    2023-10-09 bael

---
+++ baek10172.java
@@ -0,0 +1,18 @@
+package firs;
+
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+		
+	
+	}
+}
+//처음에 int형으로 선언하여 오류가 발생함, 이후 float형으로 고쳤음
---
+++ baek10926.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int boxCount=sc.nextInt();
+		int mixCount=sc.nextInt();
+		int ar[]=new int[boxCount];
+		for(int i=0;i<boxCount;i++)
+		{
+			ar[i]=i+1;
+		}
+		for(int i=0;i<mixCount;i++)
+		{
+			int first=sc.nextInt();
+			int last=sc.nextInt();
+			
+		}
+		
+		
+	
+		
+		
+	
+	}
+}
+//처음에 int형으로 선언하여 오류가 발생함, 이후 float형으로 고쳤음
