5be81679802e4cf0128b9e4d423e4a9e220c766d
juhyeok <96466824+Juhye0k@users.noreply.github.com>
2025-01-12 08:35:23 +0900

    [Silver IV] Title: 스택, Time: 188 ms, Memory: 19044 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,50 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-
-import java.util.Stack;
+import static java.sql.Types.NULL;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> st=new Stack<>();
-        int N=Integer.parseInt(br.readLine());
-        for(int i=0;i<N;i++)
-        {
-            String a[]=br.readLine().split(" ");
-            String com=a[0];
-            switch(com) {
-                case "push":
-                    int n=Integer.parseInt(a[1]);
-                    st.push(n);
-                    break;
-                case "top":
-                    if(st.isEmpty())
-                        System.out.println(-1);
-                    else
-                        System.out.println(st.peek());
-                    break;
-                case "size":
-                    System.out.println(st.size());
-                    break;
-                case "pop":
-                    if(st.isEmpty())
-                        System.out.println(-1);
-                    else
-                        System.out.println(st.pop());
-                    break;
-                case "empty":
-                    if(st.isEmpty())
-                        System.out.println(1);
-                    else
-                        System.out.println(0);
-                    break;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        Stack<Integer> stack=new Stack<>();
+        int N=Integer.parseInt(br.readLine());
+
+        for(int i=1;i<=N;i++){
+            st=new StringTokenizer(br.readLine());
+            String s1=st.nextToken();
+            if(s1.equals("push")){
+                int num=Integer.parseInt(st.nextToken());
+                stack.push(num);
+            }
+            else if(s1.equals("pop")){
+                if(stack.isEmpty())
+                {
+                    bw.write(-1+"\n");
+                    continue;
+                }
+                int num=stack.pop();
+                bw.write(num+"\n");
+            }
+            else if(s1.equals("size")){
+                bw.write(stack.size()+"\n");
+            }
+            else if(s1.equals("empty")){
+                if(stack.isEmpty()){
+                    bw.write(1+"\n");
+                }
+                else
+                    bw.write(0+"\n");
+            }
+            else{
+                if(stack.isEmpty())
+                {
+                    bw.write(-1+"\n");
+                    continue;
+                }
+                int num=stack.peek();
+                bw.write(num+"\n");
             }
         }
-
-
+        bw.flush();
+        bw.close();
 
     }
+
+
 }
