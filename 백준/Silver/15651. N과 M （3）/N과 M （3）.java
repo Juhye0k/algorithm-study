0c5da859f2e6ce7d1ac40027169e5cef66963330
juhyeok <kyoung1678@naver.com>
2025-02-19 10:08:10 +0900

    [Silver III] Title: N과 M (3), Time: 500 ms, Memory: 158172 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,42 +1,38 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int N;
-    public static int M;
-    public static int ar[];
-    static StringBuilder sb=new StringBuilder();
+    static int N,M;
+    static int[] arr;
+    static List<Integer> list;
+    static BufferedWriter bw;
     public static void main(String[] args) throws IOException {
-       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st=new StringTokenizer(br.readLine());
-       N=Integer.parseInt(st.nextToken());
-       M=Integer.parseInt(st.nextToken());
-       ar=new int[N];
-       DFS(0);
-        System.out.print(sb);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        // 자연수 N과 M이 주어짐
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
-
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        arr=new int[M];
+        list=new ArrayList<>();
+        dfs(1,0);
+        bw.flush();
+        bw.close();
 
     }
-    public static void DFS(int depth)
-    {
-        if(depth==M){
-            for(int i=0;i<M;i++)
-            {
-                sb.append(ar[i]+" ");
-            }
-            sb.append("\n");
-            return;
-        }
-        for(int i=1;i<=N;i++)
-        {
-            ar[depth]=i;
-            DFS(depth+1);
-        }
-    }
+    public static void dfs(int start,int depth) throws IOException {
+       if(depth==M){
+           for(int i=0;i<M;i++){
+               bw.write(arr[i]+" ");
+           }
+           bw.write("\n");
+           return;
+       }
+       for(int i=start;i<=N;i++){
+           arr[depth]=i;
+           dfs(start,depth+1);
 
+       }
+    }
 }
