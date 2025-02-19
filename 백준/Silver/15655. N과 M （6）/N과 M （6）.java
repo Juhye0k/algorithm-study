bf308c6e277eaa5bc842a8a60491f3bd4c1da121
juhyeok <kyoung1678@naver.com>
2025-02-19 09:23:49 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean visited[];
+    static int N,M;
+    static int[] arr;
+    static List<Integer> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 자연수 N과 M이 주어짐
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        visited=new boolean[N+1];
+        arr=new int[M];
+        list=new ArrayList<>();
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+        dfs(0);
+
+    }
+    public static void dfs(int depth) throws IOException {
+       if(depth==M){
+           for(int i=0;i<M;i++){
+               System.out.print(arr[i]+" ");
+           }
+           System.out.println();
+           return;
+       }
+       for(int i=0;i<list.size();i++){
+           int num=list.get(i);
+           if(!visited[i]){
+               if(depth>=1&&arr[depth-1]>num)
+                   continue;
+               visited[i]=true;
+               arr[depth]=num;
+               dfs(depth+1);
+               visited[i]=false;
+           }
+       }
+    }
+}
