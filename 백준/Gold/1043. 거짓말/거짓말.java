7dc1ef74ca9e7b85e5081d0fa495c893a87c4f0c
juhyeok <kyoung1678@naver.com>
2025-07-11 10:48:34 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -9,103 +9,89 @@ import java.util.*;
 
 public class Main{
 
-    static int[] parent;
-    static int[] rank;
+    static int [] parent;
+    static int [] rank;
 
     public static void makeSet(int n){
         parent = new int[n+1];
         rank = new int[n+1];
-        for(int i =1; i<=n; i++){
+        for(int i=1; i<=n; i++){
             parent[i] = i;
             rank[i] = 0;
         }
     }
-
-    public static int find(int x){
-        if(parent[x] != x){
-            parent[x] = find(parent[x]);
+    public static int find(int n){
+        if(parent[n] != n){
+            parent[n] =find(parent[n]);
         }
-        return parent[x];
+        return parent[n];
     }
-
-    public static void union(int x, int y){
-        int xRoot = find(x);
-        int yRoot = find(y);
-
-        if(xRoot != yRoot){
-            if(rank[xRoot]<rank[yRoot]){
-                parent[xRoot] = yRoot;
+    public static void union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if(aRoot != bRoot){  // 같은 집합이 아닐 때만 union
+            if(rank[aRoot] < rank[bRoot]){
+                parent[aRoot] = bRoot;
             }
-            else if(rank[xRoot]>rank[yRoot]){
-                parent[yRoot] = xRoot;
+            else if(rank[aRoot] > rank[bRoot]){
+                parent[bRoot] = aRoot;
             }
             else{
-                parent[yRoot] = xRoot;
-                rank[xRoot]++;
+                parent[bRoot] = aRoot;
+                rank[aRoot]++;
             }
         }
     }
 
-
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken()); // 사람의 수
-        int M = Integer.parseInt(st.nextToken()); // 파티의 수
-
+        // 사람의 수 N과 파티의 수 M이 주어짐
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         makeSet(N);
-
-        // 진실을 아는 사람의 수와 번호
+        // 이야기의 진실을 아는 사람의 수와 번호
+        Set<Integer> answerPeople = new HashSet<>();
         st = new StringTokenizer(br.readLine());
         int answerNum = Integer.parseInt(st.nextToken());
-        Set<Integer> answerPeople = new HashSet<>();
-
-        for(int i = 0; i<answerNum; i++){
-            answerPeople.add(Integer.parseInt(st.nextToken()));
+        if(answerNum != 0){
+            for(int i = 0; i < answerNum; i++){
+                answerPeople.add(Integer.parseInt(st.nextToken()));
+            }
         }
-
         List<List<Integer>> parties = new ArrayList<>();
-        // 각 파티마다 오는 사람의 수와 번호
-        for(int i = 0; i<M; i++){
+        // M개의 줄에 파티마다 오는 사람의 수와 번호가 같은 방식으로 주어짐
+        for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            // 파티에 오는 사람의 수
-            int num = Integer.parseInt(st.nextToken());
-            // 사람의 번호
-            List<Integer> party = new ArrayList<>();
-            for(int j = 0; j<num; j++){
-                // 해당 파티에 오는 사람들을 저장
-                party.add(Integer.parseInt(st.nextToken()));
+            int partyNum = Integer.parseInt(st.nextToken());
+            List<Integer> partyPeople = new ArrayList<>();
+            for(int j=0; j<partyNum; j++){
+                partyPeople.add(Integer.parseInt(st.nextToken()));
             }
-            parties.add(party);
-            // 같은 파티에 참석한 사람들을 같은 파티로 연결
-
-            for(int j=0; j<party.size()-1; j++){
-                union(party.get(j), party.get(j+1));
+            for(int j=0; j<partyPeople.size()-1; j++){
+                union(partyPeople.get(j), partyPeople.get(j+1));
             }
+            parties.add(partyPeople);
         }
         Set<Integer> connectedTruth = new HashSet<>();
         for(int truthPerson : answerPeople){
             int root = find(truthPerson);
-            for(int i=1 ;i<=N; i++){
+            for(int i=1; i<=N; i++){
                 if(find(i) == root){
                     connectedTruth.add(i);
                 }
             }
         }
-
         int answer = 0;
         for(List<Integer> party : parties){
-            boolean canLie = true;
+            boolean isTrue = true;
             for(int person : party){
                 if(connectedTruth.contains(person)){
-                    canLie =false;
+                    isTrue = false;
                     break;
                 }
             }
-            if(canLie){
+            if(isTrue){
                 answer++;
             }
         }
