89f2013b8069b79bd253dfc45e376f3ded102e98
juhyeok <kyoung1678@naver.com>
2025-12-07 11:47:26 +0900

    [Silver I] Title: 토쟁이의 등굣길, Time: 100 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/12785. 토쟁이의 등굣길/토쟁이의 등굣길.java
+++ 백준/Silver/12785. 토쟁이의 등굣길/토쟁이의 등굣길.java
@@ -20,40 +20,32 @@ class Node {
 public class Main {
 
     static int w,h;
-    static int[] dx= {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static int goal;
+    static final int mod =1000007;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new  StringTokenizer(br.readLine());
 
         w = Integer.parseInt(st.nextToken());
         h = Integer.parseInt(st.nextToken());
-
-        st= new StringTokenizer(br.readLine());
-
+        st =  new  StringTokenizer(br.readLine());
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
-        BigInteger path1 = solveDP(x, y);
-        BigInteger path2 = solveDP(w-x+1, h-y+1);
-        System.out.println(path1.multiply(path2).remainder(new BigInteger("1000007")));
+        long distance1 = solveDP(x, y);
+        long distance2 = solveDP(w-x+1, h-y+1);
+        System.out.println(distance1*distance2%mod);
+
     }
 
+    public static long solveDP(int x, int y) {
+        long[][] dp = new long[w+1][h+1];
 
-
-    public static BigInteger solveDP(int width, int height) {
-        BigInteger[][] dp = new BigInteger[height+1][width+1];
-        for (int i = 0; i <= height; i++) dp[i][0] = BigInteger.ZERO;
-        for (int j = 0; j <= width; j++) dp[0][j] = BigInteger.ZERO;
-        for(int i=1; i<=height; i++) {
-            for(int j=1; j<=width; j++) {
+        for(int i=1; i<=x; i++) {
+            for(int j=1; j<=y; j++) {
                 if(i==1 && j==1)
-                    dp[i][j] = BigInteger.ONE;
+                    dp[i][j] = 1;
                 else
-                    dp[i][j] = dp[i-1][j].add(dp[i][j-1]);
-            }
+                    dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod;            }
         }
-        return dp[height][width];
+        return dp[x][y];
     }
 }
