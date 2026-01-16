1bb6071865df99f58f7989b2225b7918d93493e0
juhyeok <kyoung1678@naver.com>
2026-01-16 10:37:20 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 100 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int mod = 1000000000;
+        int N = Integer.parseInt(br.readLine());
+        long[][] dp = new long[N+1][10];
+
+        for(int i=1; i<=9; i++) {
+            dp[1][i] = 1;
+        }
+        if(N==1) {
+            System.out.println(9);
+            return;
+        }
+
+        for(int i=2; i<=N; i++) {
+            for(int j=0; j<=9; j++) {
+                if(j==0) {
+                    dp[i][j] = dp[i-1][1]%mod;
+                }
+                else if (j==9) {
+                    dp[i][j] = dp[i-1][8]%mod;
+                }
+                else {
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%mod;
+                }
+            }
+        }
+        long result = 0;
+        for(int i=0; i<=9; i++) {
+            result = (result+dp[N][i])%mod;
+        }
+        System.out.println(result);
+    }
+}
